1eee7aa2feaa79ee794699200db2225e4de0569b
YGVKN/ZHUZHA <denisyagovkin@mail.ru>
2024-08-18 22:50:26 +0300

    PWA Example 2024-08-18 22:50:26
    Prepare origin

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "v1.0.1";
+const VERSION = "v2";
 
 // The name of the cache
 const CACHE_NAME = `PWA-EXAMPLE-${VERSION}`;
@@ -12,53 +12,70 @@ const APP_STATIC_RESOURCES = [
   "/icons/wheel.svg",
 ];
 
-// On install, cache the static resources
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
       cache.addAll(APP_STATIC_RESOURCES);
-    })()
+    })
   );
 });
 
-// delete old caches on activate
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    (async () => {
-      const names = await caches.keys();
-      await Promise.all(
-        names.map((name) => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      );
-      await clients.claim();
-    })()
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
   );
 });
 
-// On fetch, intercept server requests
-// and respond with cached responses instead of going to network
-self.addEventListener("fetch", (event) => {
-  // As a single page app, direct app to always go to cached home page.
-  if (event.request.mode === "navigate") {
-    event.respondWith(caches.match("/"));
-    return;
-  }
-
-  // For all other requests, go to the cache first, and then the network.
-  event.respondWith(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      const cachedResponse = await cache.match(event.request);
-      if (cachedResponse) {
-        // Return the cached response if it's available.
-        return cachedResponse;
-      }
-      // If resource isn't in the cache, return a 404.
-      return new Response(null, { status: 404 });
-    })()
-  );
-});
+//// On install, cache the static resources
+//self.addEventListener("install", (event) => {
+//  event.waitUntil(
+//    (async () => {
+//      const cache = await caches.open(CACHE_NAME);
+//      cache.addAll(APP_STATIC_RESOURCES);
+//    })()
+//  );
+//});
+//
+//// delete old caches on activate
+//self.addEventListener("activate", (event) => {
+//  event.waitUntil(
+//    (async () => {
+//      const names = await caches.keys();
+//      await Promise.all(
+//        names.map((name) => {
+//          if (name !== CACHE_NAME) {
+//            return caches.delete(name);
+//          }
+//        })
+//      );
+//      await clients.claim();
+//    })()
+//  );
+//});
+//
+//// On fetch, intercept server requests
+//// and respond with cached responses instead of going to network
+//self.addEventListener("fetch", (event) => {
+//  // As a single page app, direct app to always go to cached home page.
+//  if (event.request.mode === "navigate") {
+//    event.respondWith(caches.match("/"));
+//    return;
+//  }
+//
+//  // For all other requests, go to the cache first, and then the network.
+//  event.respondWith(
+//    (async () => {
+//      const cache = await caches.open(CACHE_NAME);
+//      const cachedResponse = await cache.match(event.request);
+//      if (cachedResponse) {
+//        // Return the cached response if it's available.
+//        return cachedResponse;
+//      }
+//      // If resource isn't in the cache, return a 404.
+//      return new Response(null, { status: 404 });
+//    })()
+//  );
+//});
