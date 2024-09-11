d8315205675aaaf5f9ba2c053bb4d2ea47f8de2e
YGVKN/ZHUZHA <denisyagovkin@mail.ru>
2024-09-11 19:53:57 +0300

    Origin v2.7 2024-09-11 19:53:57
    PWA

--- sw.js
+++ sw.js
@@ -1,30 +1,39 @@
-const VERSION = "v2.5";
+const VERSION = "v2.7";
 
 const CACHE_NAME = `YGVKN-PWA-${VERSION}`;
-
+//APPSHELLFILES
 const APP_STATIC_RESOURCES = [
   "/",
   "/index.html",
-  "/favicon.ico",
-  "/app.webmanifest",
-  "/app.js",
-  "/icons",
+
   "/style.css",
+  "/app.js",
+  "/icons"
+
+  //"/favicon.ico",
+  //"/app.webmanifest",
 ];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       cache.addAll(APP_STATIC_RESOURCES);
-    })
+    })); });
+
+self.addEventListener("activate", (event) => {
+  const cacheAllowlist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.forEach((cache, cacheName) => {
+      if (!cacheAllowlist.includes(cacheName)) {
+        return caches.delete(cacheName);
+      }
+    }),
   );
 });
-
 
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then(res => {
       return res || fetch(fetchEvent.request);
-    })
-  );
-});
+    })); });
